4dc4efffe201634f01eb2955fab4fe2a53cccd36
purpla-fantastic <pdp330528@gmail.com>
2026-06-19 12:28:35 +0300

    Fixed header file

--- teng.h
+++ teng.h
@@ -1,10 +1,15 @@
-#ifndef TENG-HEADER_H
-#define TENG-HEADER_H
+#ifndef TENG_H
+#define TENG_H
 
 typedef struct {
     const int width;
     const int height;
     char* buffer;
 } area_t;
+
+area_t create_area(int width, int height);
+void update(area_t area);
+void change_buffer(area_t *area, char buffer[]);
+void update_with_buffer(area_t *area, char buffer[]);
 
 #endif
